1dd526205c537ab4143381b3ac38801e5530ed5a
Scott Plunkett <iamretiform@github.com>
2017-07-11 10:05:25 -0500

    initial App component + Dashboard component classes

--- forkasta-fast/src/App.js
+++ forkasta-fast/src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+const POPULAR_PLACES = [
+  { name: "CHICAGO", zip: "60618" },
+  { name: "STAGECOACH", zip: "92849" },
+  { name: "ROME", zip: "none" },
+  { name: "CAPE CANAVERAL", zip: "32920" }
+];
+
+class Dashboard extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-            {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="Dashboard">
+        <h2>Welcome to <br></br>
+        <b>···ForkASTA···ƒAST™···</b></h2>
+        <h3>——Displaying weather for–—</h3> 
+        <text>City: {this.props.name}</text><br></br>
+        <text>Zip: {this.props.zip}</text>
       </div>
     );
   }
 }
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Dashboard name={"CHICAGO"} zip={"60618"} />
+          {POPULAR_PLACES.map((place, index) => (
+            <button
+              key={index}
+              onClick={() => {
+                console.log('Clicked index '+index);
+              }}
+            >
+            {place.name}
+            </button>
+          ))}
+        </div>
+      );
+    }
+  }
+
 export default App;
+
+
