bb0d39064495b7913ee5522f7b172490226e357c
Scott Plunkett <iamretiform@github.com>
2017-07-13 07:23:10 -0500

    organize dataflow with redux, then UI

--- forkasta-fast/src/App.js
+++ forkasta-fast/src/App.js
@@ -10,69 +10,102 @@ const PLACES = [
 ];
 
 
-
-
-class Dashboard extends Component {
+class Current extends Component {
   constructor() {
     super();
     this.state = {
-      weatherData: null
+      currentData: null
     };
   }
   componentDidMount() {
     const zip = this.props.zip;
-    const city = this.props.city;
-    const country = this.props.country;
     const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
                 zip +
                 "&appid=e9ed86c3f9edf086eb52665e26c72844&units=imperial";
     fetch(URL).then(res => res.json(
       )).then(json => {
-      this.setState({ weatherData: json });
+      this.setState({ currentData: json });
     });
   }
   render() {
-    const weatherData = this.state.weatherData;
-    if (!weatherData) return <div>Loading</div>;
-    const weather = weatherData.weather[0];
+    const currentData = this.state.currentData;
+    if (!currentData) return <div>Loading</div>;
+    const weather = currentData.weather[0];
     const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+    console.log(currentData)
     return (
       <div>
         <h1>
-          {weather.main} in {weatherData.city}, {weatherData.country}
-          <img src={iconUrl} alt={weatherData.description} />
+          {weather.main} in {currentData.name}
+          <img src={iconUrl} alt={currentData.description} />
         </h1>
-        <p>Current: {weatherData.main.temp}°</p>
-        <p>High: {weatherData.main.temp_max}°</p>
-        <p>Low: {weatherData.main.temp_min}°</p>
-        <p>Wind Speed: {weatherData.wind.speed} mi/hr</p>
+        <p>Current: {currentData.main.temp}°</p>
+        <p>High: {currentData.main.temp_max}°</p>
+        <p>Low: {currentData.main.temp_min}°</p>
+        <p>Wind Speed: {currentData.wind.speed} mi/hr</p>
       </div>
     );
   }
 }
 
+
+  class Forecast extends Component {
+    constructor() {
+      super();
+      this.state = {
+        forecastData: null
+      };
+    }
+    componentDidMount() {
+      const zip = this.props.zip;
+      const URL = "http://api.openweathermap.org/data/2.5/forecast?q=" +
+                  zip +
+                  "&appid=e9ed86c3f9edf086eb52665e26c72844&units=imperial";
+      fetch(URL).then(res => res.json(
+        )).then(json => {
+        this.setState({ forecastData: json });
+      });
+    }
+    render() {
+      const forecastData = this.state.forecastData;
+      if (!forecastData) return <div>Loading</div>;
+
+
+      console.log(forecastData)
+      return (
+        <div>
+        {forecastData.cnt}
+        </div>
+      );
+    }
+  }
+
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      activePlace: 0
+      activePlace: 0,
+      forecastPlace: -1,
     };
   }
   render() {
     const activePlace = this.state.activePlace;
+    const forecastPlace = this.state.forecastPlace;
     return (
       <div className="App">
         {PLACES.map((place, index) => (
           <button
             key={index}
             onClick={() => {
-              this.setState({ activePlace: index });
+              this.setState({ activePlace: index }, { forecastPlace: index }, { name: place.name });
             }}
           >
             {place.city}
           </button>
         ))}
-        <Dashboard key={activePlace} zip={PLACES[activePlace].zip} />
+        <Current key={activePlace} zip={PLACES[activePlace].zip} />
+        <Forecast key={forecastPlace} zip={PLACES[activePlace].zip} />
       </div>
     );
   }
